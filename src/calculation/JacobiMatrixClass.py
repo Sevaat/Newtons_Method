e7ca2e005f84a760124067b4bda9f129af5f8fbf
tkachenko <sevaatmail@gmail.com>
2025-01-10 18:19:31 +0500

    Добавление
    Расчет матрицы Якоби в полярной системе координат. Тесты.

--- src/calculation/JacobiMatrixClass.py
+++ src/calculation/JacobiMatrixClass.py
@@ -1,56 +1,172 @@
 from abc import ABC
 import numpy
 import src.utils.GlobalVariables as GV
+from math import *
 
 
 class JacobiMatrix(ABC):
     @staticmethod
     def get_jacobi_matrix_cartesian(y_matrix: numpy.ndarray) -> numpy.ndarray:
         """
-        Получение матрицы Якоби
+        Получение матрицы Якоби в декартовых координатах
         :param y_matrix: матрица собственных и взаимных проводимостей узлов
         :return: матрица Якоби
         """
-        node_count = len(GV.nodes)
         jacobi_matrix = []
-        for i in range(node_count):
+        for i in range(len(GV.nodes)): # номер мощности
             if GV.nodes[i].type_node != 'ИП':
-                dp_u = []
-                dq_u = []
-                for j in range(node_count):
-                    if GV.nodes[j].type_node != 'ИП':
-                        pur = 0
-                        pui = 0
-                        qur = 0
-                        qui = 0
-                        if i == j:
-                            for k in range(node_count):
-                                if k != j:
-                                    pur += y_matrix[i, k].real * GV.nodes[k].voltage.real + y_matrix[i, k].imag * \
-                                           GV.nodes[k].voltage.imag
-                                    pui += y_matrix[i, k].real * GV.nodes[k].voltage.imag - y_matrix[i, k].imag * \
-                                           GV.nodes[k].voltage.real
-                                    qur += y_matrix[i, k].real * GV.nodes[k].voltage.imag - y_matrix[i, k].imag * \
-                                           GV.nodes[k].voltage.real
-                                    qui += y_matrix[i, k].real * GV.nodes[k].voltage.real + y_matrix[i, k].imag * \
-                                           GV.nodes[k].voltage.imag
-                            pur = -2 * y_matrix[i, i].real * GV.nodes[i].voltage.real - pur
-                            pui = -2 * y_matrix[i, i].real * GV.nodes[i].voltage.imag - pui
-                            qur = 2 * y_matrix[i, i].imag * GV.nodes[i].voltage.real - qur
-                            qui = 2 * y_matrix[i, i].imag * GV.nodes[i].voltage.imag + qui
-                        else:
-                            pur = -y_matrix[i, j].real * GV.nodes[j].voltage.real + y_matrix[i, j].imag * GV.nodes[
-                                j].voltage.imag
-                            pui = -y_matrix[i, j].imag * GV.nodes[j].voltage.real - y_matrix[i, j].real * GV.nodes[
-                                j].voltage.imag
-                            qur = y_matrix[i, j].imag * GV.nodes[j].voltage.real + y_matrix[i, j].real * GV.nodes[
-                                j].voltage.imag
-                            qui = -y_matrix[i, j].real * GV.nodes[j].voltage.real + y_matrix[i, j].imag * GV.nodes[
-                                j].voltage.imag
-                        dp_u.append(pur)
-                        dp_u.append(pui)
-                        dq_u.append(qur)
-                        dq_u.append(qui)
-                jacobi_matrix.append(dp_u)
-                jacobi_matrix.append(dq_u)
+                row_pi_jacobi_matrix = JacobiMatrix.get_row_pi_jacobi_matrix_cartesian(i, y_matrix)
+                jacobi_matrix.append(row_pi_jacobi_matrix)
+                row_qi_jacobi_matrix = JacobiMatrix.get_row_qi_jacobi_matrix_cartesian(i, y_matrix)
+                jacobi_matrix.append(row_qi_jacobi_matrix)
         return numpy.array(jacobi_matrix)
+
+    @staticmethod
+    def get_row_pi_jacobi_matrix_cartesian(i: int, y_matrix: numpy.ndarray):
+        row_pi_jm = []
+        for j in range(len(GV.nodes)): # номер напряжения
+            if GV.nodes[j].type_node != 'ИП':
+                dpi_du = JacobiMatrix.get_dpi_du_cartesian(i, j, y_matrix)
+                row_pi_jm.append(dpi_du[0])
+                row_pi_jm.append(dpi_du[1])
+        return row_pi_jm
+
+    @staticmethod
+    def get_dpi_du_cartesian(i: int, j: int, y_matrix: numpy.ndarray):
+        if i == j:
+            dpi_dui_real = [0, 0]
+            dpi_dui_imag = [0, 0]
+            dpi_dui_real[0] = 2 * y_matrix[i, i].real * GV.nodes[i].voltage.real
+            dpi_dui_imag[0] = 2 * y_matrix[i, i].real * GV.nodes[i].voltage.imag
+            for k in range(len(GV.nodes)): # номер позиции под знаком суммы
+                if k != i:
+                    dpi_dui_real[1] += y_matrix[i, k].real * GV.nodes[k].voltage.real
+                    dpi_dui_real[1] -= y_matrix[i, k].imag * GV.nodes[k].voltage.imag
+                    dpi_dui_imag[1] += y_matrix[i, k].real * GV.nodes[k].voltage.imag
+                    dpi_dui_imag[1] += y_matrix[i, k].imag * GV.nodes[k].voltage.real
+            dpi_dui_real = dpi_dui_real[0] + dpi_dui_real[1]
+            dpi_dui_imag = dpi_dui_imag[0] + dpi_dui_imag[1]
+            return dpi_dui_real, dpi_dui_imag
+        else:
+            dpi_duj_real = y_matrix[i, j].real * GV.nodes[i].voltage.real
+            dpi_duj_real += y_matrix[i, j].imag * GV.nodes[i].voltage.imag
+            dpi_duj_imag = y_matrix[i, j].real * GV.nodes[i].voltage.imag
+            dpi_duj_imag -= y_matrix[i, j].imag * GV.nodes[i].voltage.real
+            return dpi_duj_real, dpi_duj_imag
+
+    @staticmethod
+    def get_row_qi_jacobi_matrix_cartesian(i: int, y_matrix: numpy.ndarray):
+        row_qi_jm = []
+        for j in range(len(GV.nodes)):  # номер напряжения
+            if GV.nodes[j].type_node != 'ИП':
+                dqi_du = JacobiMatrix.get_dqi_du_cartesian(i, j, y_matrix)
+                row_qi_jm.append(dqi_du[0])
+                row_qi_jm.append(dqi_du[1])
+        return row_qi_jm
+
+    @staticmethod
+    def get_dqi_du_cartesian(i: int, j: int, y_matrix: numpy.ndarray):
+        if i == j:
+            dqi_dui_real = [0, 0]
+            dqi_dui_imag = [0, 0]
+            dqi_dui_real[0] = - 2 * y_matrix[i, i].imag * GV.nodes[i].voltage.real
+            dqi_dui_imag[0] = - 2 * y_matrix[i, i].imag * GV.nodes[i].voltage.imag
+            for k in range(len(GV.nodes)):  # номер позиции под знаком суммы
+                if k != i:
+                    dqi_dui_real[1] += y_matrix[i, k].real * GV.nodes[k].voltage.imag
+                    dqi_dui_real[1] += y_matrix[i, k].imag * GV.nodes[k].voltage.real
+                    dqi_dui_imag[1] += y_matrix[i, k].real * GV.nodes[k].voltage.real
+                    dqi_dui_imag[1] -= y_matrix[i, k].imag * GV.nodes[k].voltage.imag
+            dqi_dui_real = dqi_dui_real[0] - dqi_dui_real[1]
+            dqi_dui_imag = dqi_dui_imag[0] + dqi_dui_imag[1]
+            return dqi_dui_real, dqi_dui_imag
+        else:
+            dqi_duj_real = y_matrix[i, j].real * GV.nodes[i].voltage.imag
+            dqi_duj_real -= y_matrix[i, j].imag * GV.nodes[i].voltage.real
+            dqi_duj_imag = - y_matrix[i, j].real * GV.nodes[i].voltage.real
+            dqi_duj_imag -= y_matrix[i, j].imag * GV.nodes[i].voltage.imag
+        return dqi_duj_real, dqi_duj_imag
+
+    @staticmethod
+    def get_jacobi_matrix_polar(y_matrix: numpy.ndarray) -> numpy.ndarray:
+        """
+        Получение матрицы Якоби в полярных координатах
+        :param y_matrix: матрица собственных и взаимных проводимостей узлов
+        :return: матрица Якоби
+        """
+        jacobi_matrix = []
+        for i in range(len(GV.nodes)):  # номер мощности
+            if GV.nodes[i].type_node != 'ИП':
+                row_pi_jacobi_matrix = JacobiMatrix.get_row_pi_jacobi_matrix_polar(i, y_matrix)
+                jacobi_matrix.append(row_pi_jacobi_matrix)
+                row_qi_jacobi_matrix = JacobiMatrix.get_row_qi_jacobi_matrix_polar(i, y_matrix)
+                jacobi_matrix.append(row_qi_jacobi_matrix)
+        return numpy.array(jacobi_matrix)
+
+    @staticmethod
+    def get_row_pi_jacobi_matrix_polar(i: int, y_matrix: numpy.ndarray):
+        row_pi_jm = []
+        for j in range(len(GV.nodes)):  # номер напряжения
+            if GV.nodes[j].type_node != 'ИП':
+                dpi_du = JacobiMatrix.get_dpi_du_polar(i, j, y_matrix)
+                row_pi_jm.append(dpi_du[0])
+                row_pi_jm.append(dpi_du[1])
+        return row_pi_jm
+
+    @staticmethod
+    def get_dpi_du_polar(i: int, j: int, y_matrix: numpy.ndarray):
+        u_i = GV.nodes[i].get_polar_voltage()
+        b_i = GV.nodes[i].get_polar_angle()
+        u_j = GV.nodes[j].get_polar_voltage()
+        b_j = GV.nodes[j].get_polar_angle()
+        if i == j:
+            dpi_dui = [0, 0]
+            dpi_dbi = 0
+            dpi_dui[0] = 2 * y_matrix[i, i].real * u_i
+            for k in range(len(GV.nodes)):  # номер позиции под знаком суммы
+                if k != i:
+                    u_k = GV.nodes[k].get_polar_voltage()
+                    b_k = GV.nodes[k].get_polar_angle()
+                    dpi_dui[1] += u_k * (y_matrix[i, k].real * cos(b_i - b_k) + y_matrix[i, k].imag * sin(b_i - b_k))
+                    dpi_dbi += u_k * (y_matrix[i, k].real * sin(b_k - b_i) + y_matrix[i, k].imag * cos(b_i - b_k))
+            dpi_dui = dpi_dui[0] + dpi_dui[1]
+            dpi_dbi = u_i * dpi_dbi
+            return dpi_dui, dpi_dbi
+        else:
+            dpi_duj = u_i * (y_matrix[i, j].real * cos(b_i - b_j) + y_matrix[i, j].imag * sin(b_i - b_j))
+            dpi_dbj = u_i * u_j * (y_matrix[i, j].real * sin(b_i - b_j) - y_matrix[i, j].imag * cos(b_i - b_j))
+            return dpi_duj, dpi_dbj
+
+    @staticmethod
+    def get_row_qi_jacobi_matrix_polar(i: int, y_matrix: numpy.ndarray):
+        row_qi_jm = []
+        for j in range(len(GV.nodes)):  # номер напряжения
+            if GV.nodes[j].type_node != 'ИП':
+                dqi_du = JacobiMatrix.get_dqi_du_polar(i, j, y_matrix)
+                row_qi_jm.append(dqi_du[0])
+                row_qi_jm.append(dqi_du[1])
+        return row_qi_jm
+
+    @staticmethod
+    def get_dqi_du_polar(i: int, j: int, y_matrix: numpy.ndarray):
+        u_i = GV.nodes[i].get_polar_voltage()
+        b_i = GV.nodes[i].get_polar_angle()
+        u_j = GV.nodes[j].get_polar_voltage()
+        b_j = GV.nodes[j].get_polar_angle()
+        if i == j:
+            dqi_dui = [0, 0]
+            dqi_dbi = 0
+            dqi_dui[0] = - 2 * y_matrix[i, i].imag * u_i
+            for k in range(len(GV.nodes)):  # номер позиции под знаком суммы
+                if k != i:
+                    u_k = GV.nodes[k].get_polar_voltage()
+                    b_k = GV.nodes[k].get_polar_angle()
+                    dqi_dui[1] += u_k * (y_matrix[i, k].real * sin(b_i - b_j) - y_matrix[i, k].imag * cos(b_i - b_k))
+                    dqi_dbi += u_k * (y_matrix[i, k].real * cos(b_i - b_k) - y_matrix[i, k].imag * sin(b_k - b_i))
+            dqi_dui = dqi_dui[0] + dqi_dui[1]
+            dqi_dbi = u_i * dqi_dbi
+            return dqi_dui, dqi_dbi
+        else:
+            dqi_duj = u_i * (y_matrix[i, j].real * sin(b_i - b_j) - y_matrix[i, j].imag * cos(b_i - b_j))
+            dqi_dbj = u_i * u_j * (- y_matrix[i, j].real * cos(b_i - b_j) - y_matrix[i, j].imag * sin(b_i - b_j))
+            return dqi_duj, dqi_dbj
--- src/tests/calculation_tests/JacobiMatrixClass_tests.py
+++ src/tests/calculation_tests/JacobiMatrixClass_tests.py
@@ -4,15 +4,16 @@ from src.utils.FileManagerClass import FileManager
 import src.utils.GlobalVariables as GV
 from src.calculation.YMatrixClass import YMatrix
 
-filepath = "../data_tests/Data_Test_1.txt"
+filepath_1 = "../data_tests/Data_Test_1.txt"
+filepath_2 = "../data_tests/Data_Test_2.txt"
 
 
 class JacobiMatrixClassTests(unittest.TestCase):
     def test_get_jacobi_matrix_cartesian(self):
-        GV.OPTIONS = FileManager._FileManager__get_options(filepath)
-        GV.nodes = FileManager._FileManager__get_nodes(filepath)
-        GV.branches = FileManager._FileManager__get_branches(filepath)
-        GV.NEWTONMETHOD = FileManager._FileManager__get_newton_method(filepath)
+        GV.OPTIONS = FileManager._FileManager__get_options(filepath_1)
+        GV.nodes = FileManager._FileManager__get_nodes(filepath_1)
+        GV.branches = FileManager._FileManager__get_branches(filepath_1)
+        GV.NEWTONMETHOD = FileManager._FileManager__get_newton_method(filepath_1)
         GV.nodes[0].voltage = complex(115, 0)
 
         y_m = YMatrix.get_y_matrix()
@@ -26,10 +27,36 @@ class JacobiMatrixClassTests(unittest.TestCase):
         ]
 
         expected_result = [
-            [-3.617, 8.393, 1.517, -3.793],
-            [-7.993, -3.817, 3.793, 1.517],
-            [1.517, -3.793, -2.917, 6.860],
-            [3.793, 1.517, -6.593, -3.051]
+            [3.617, 8.393, -1.517, -3.793],
+            [7.993, -3.817, -3.793, 1.517],
+            [-1.517, -3.793, 2.917, 6.860],
+            [-3.793, 1.517, 6.593, -3.051]
+        ]
+
+        self.assertEqual(result, expected_result)
+
+    def test_get_jacobi_matrix_polar(self):
+        GV.OPTIONS = FileManager._FileManager__get_options(filepath_2)
+        GV.nodes = FileManager._FileManager__get_nodes(filepath_2)
+        GV.branches = FileManager._FileManager__get_branches(filepath_2)
+        GV.NEWTONMETHOD = FileManager._FileManager__get_newton_method(filepath_2)
+        GV.nodes[0].voltage = complex(115, 0)
+
+        y_m = YMatrix.get_y_matrix()
+        j_m = numpy.round(GV.NEWTONMETHOD.jacoby_matrix_calculation(y_m), 3)
+
+        result = [
+            [j_m[0][0], j_m[0][1], j_m[0][2], j_m[0][3]],
+            [j_m[1][0], j_m[1][1], j_m[1][2], j_m[1][3]],
+            [j_m[2][0], j_m[2][1], j_m[2][2], j_m[2][3]],
+            [j_m[3][0], j_m[3][1], j_m[3][2], j_m[3][3]]
+        ]
+
+        expected_result = [
+            [3.617, 923.241, -1.517, -417.241],
+            [7.993, -419.897, -3.793, 166.897],
+            [-1.517, -417.241, 2.917, 754.575],
+            [-3.793, 166.897, 6.593, -335.563]
         ]
 
         self.assertEqual(result, expected_result)
