21e3e76b2e3be739287f555a918ca256e4d196c2
tkachenko <sevaatmail@gmail.com>
2025-01-10 18:22:16 +0500

    Добавление
    Корректировка напряжений при расчете в полярной системе координат. Тесты.

--- src/calculation/VoltageCorrectionClass.py
+++ src/calculation/VoltageCorrectionClass.py
@@ -6,7 +6,7 @@ class VoltageCorrection(ABC):
     @staticmethod
     def voltage_correction_cartesian(delta_voltage):
         """
-        Корректировка напряжений узлов
+        Корректировка напряжений узлов в декартовых координатах
         :param delta_voltage: корректировки напряжений узлов
         :return: скорректированные напряжения узлов
         """
@@ -14,4 +14,19 @@ class VoltageCorrection(ABC):
         for i in range(len(GV.nodes)):
             if GV.nodes[i].type_node != 'ИП':
                 GV.nodes[i].voltage = GV.nodes[i].voltage + delta_voltage[j]
+                j += 1
+
+    @staticmethod
+    def voltage_correction_polar(delta_voltage):
+        """
+        Корректировка напряжений узлов в полярных координатах
+        :param delta_voltage: корректировки напряжений узлов
+        :return: скорректированные напряжения узлов
+        """
+        j = 0
+        for i in range(len(GV.nodes)):
+            if GV.nodes[i].type_node != 'ИП':
+                voltage = GV.nodes[i].get_polar_voltage() + delta_voltage[j][0]
+                angle = GV.nodes[i].get_polar_angle() + delta_voltage[j][1]
+                GV.nodes[i].polar_to_cartesian(voltage, angle)
                 j += 1
--- src/tests/calculation_tests/VoltageCorrectionClass_tests.py
+++ src/tests/calculation_tests/VoltageCorrectionClass_tests.py
@@ -3,15 +3,16 @@ from src.utils.FileManagerClass import FileManager
 import src.utils.GlobalVariables as GV
 from src.calculation.YMatrixClass import YMatrix
 
-filepath = "../data_tests/Data_Test_1.txt"
+filepath_1 = "../data_tests/Data_Test_1.txt"
+filepath_2 = "../data_tests/Data_Test_2.txt"
 
 
 class VoltageCorrectionClassTests(unittest.TestCase):
     def test_voltage_correction_cartesian(self):
-        GV.OPTIONS = FileManager._FileManager__get_options(filepath)
-        GV.nodes = FileManager._FileManager__get_nodes(filepath)
-        GV.branches = FileManager._FileManager__get_branches(filepath)
-        GV.NEWTONMETHOD = FileManager._FileManager__get_newton_method(filepath)
+        GV.OPTIONS = FileManager._FileManager__get_options(filepath_1)
+        GV.nodes = FileManager._FileManager__get_nodes(filepath_1)
+        GV.branches = FileManager._FileManager__get_branches(filepath_1)
+        GV.NEWTONMETHOD = FileManager._FileManager__get_newton_method(filepath_1)
         GV.nodes[0].voltage = complex(115, 0)
 
         y_m = YMatrix.get_y_matrix()
@@ -23,7 +24,27 @@ class VoltageCorrectionClassTests(unittest.TestCase):
         result = [GV.nodes[1].voltage, GV.nodes[2].voltage]
         result = [complex(round(r.real, 3), round(r.imag, 3)) for r in result]
 
-        expected_result = [complex(115.915, -0.308), complex(110.098, 4.227)]
+        expected_result = [complex(115.915, 0.308), complex(110.098, -4.227)]
+
+        self.assertEqual(result, expected_result)
+
+    def test_voltage_correction_polar(self):
+        GV.OPTIONS = FileManager._FileManager__get_options(filepath_2)
+        GV.nodes = FileManager._FileManager__get_nodes(filepath_2)
+        GV.branches = FileManager._FileManager__get_branches(filepath_2)
+        GV.NEWTONMETHOD = FileManager._FileManager__get_newton_method(filepath_2)
+        GV.nodes[0].voltage = complex(115, 0)
+
+        y_m = YMatrix.get_y_matrix()
+        i_s = GV.NEWTONMETHOD.imbalance_calculation(y_m)
+        j_m = GV.NEWTONMETHOD.jacoby_matrix_calculation(y_m)
+        d_v = GV.NEWTONMETHOD.linear_algebraic_equation_solver(i_s, j_m)
+        GV.NEWTONMETHOD.voltage_correction(d_v)
+
+        result = [GV.nodes[1].voltage, GV.nodes[2].voltage]
+        result = [complex(round(r.real, 3), round(r.imag, 3)) for r in result]
+
+        expected_result = [complex(115.915, 0.325), complex(110.017, -4.23)]
 
         self.assertEqual(result, expected_result)
 
