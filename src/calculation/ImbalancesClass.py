66a3fe2576aeea5a23a603f17ffacd67f4b73b42
tkachenko <sevaatmail@gmail.com>
2025-01-10 18:19:04 +0500

    Добавление
    Расчет небаланса мощности в полярной системе координат. Тесты.

--- src/calculation/ImbalancesClass.py
+++ src/calculation/ImbalancesClass.py
@@ -1,13 +1,14 @@
 from abc import ABC
 import numpy
 import src.utils.GlobalVariables as GV
+from math import *
 
 
 class Imbalances(ABC):
     @staticmethod
     def get_imbalances_s_cartesian(y_matrix: numpy.ndarray) -> list:
         """
-        Получение небалансов полной мощности узлов
+        Получение небалансов полной мощности узлов в декартовых координатах
         :param y_matrix: матрица собственных и взаимных проводимостей узлов
         :return: список небалансов полной мощности узлов
         """
@@ -22,22 +23,56 @@ class Imbalances(ABC):
             else:
                 s_power = GV.nodes[i].s_power
             p = [0, 0, 0]
-            p[0] = -s_power.real - y_matrix[i, i].real * abs(GV.nodes[i].voltage) ** 2
+            p[0] = s_power.real + y_matrix[i, i].real * GV.nodes[i].get_polar_voltage() ** 2
             q = [0, 0, 0]
-            q[0] = -s_power.imag + y_matrix[i, i].imag * abs(GV.nodes[i].voltage) ** 2
+            q[0] = s_power.imag - y_matrix[i, i].imag * GV.nodes[i].get_polar_voltage() ** 2
             for j in range(node_count):
                 if j != i:
-                    p[1] += y_matrix[i, j].real * GV.nodes[j].voltage.real + y_matrix[i, j].imag * GV.nodes[
-                        j].voltage.imag
-                    p[2] += -y_matrix[i, j].imag * GV.nodes[j].voltage.real + y_matrix[i, j].real * GV.nodes[
-                        j].voltage.imag
-                    q[1] += -y_matrix[i, j].imag * GV.nodes[j].voltage.real + y_matrix[i, j].real * GV.nodes[
-                        j].voltage.imag
-                    q[2] += y_matrix[i, j].imag * GV.nodes[j].voltage.imag + y_matrix[i, j].real * GV.nodes[
-                        j].voltage.real
-            p[1] = - GV.nodes[i].voltage.real * p[1]
-            p[2] = - GV.nodes[i].voltage.imag * p[2]
-            q[1] = - GV.nodes[i].voltage.real * q[1]
-            q[2] = GV.nodes[i].voltage.imag * q[2]
+                    p[1] += y_matrix[i, j].real * GV.nodes[j].voltage.real
+                    p[1] -= y_matrix[i, j].imag * GV.nodes[j].voltage.imag
+                    p[2] += y_matrix[i, j].real * GV.nodes[j].voltage.imag
+                    p[2] += y_matrix[i, j].imag * GV.nodes[j].voltage.real
+                    q[1] += y_matrix[i, j].real * GV.nodes[j].voltage.real
+                    q[1] -= y_matrix[i, j].imag * GV.nodes[j].voltage.imag
+                    q[2] += y_matrix[i, j].real * GV.nodes[j].voltage.imag
+                    q[2] += y_matrix[i, j].imag * GV.nodes[j].voltage.real
+            p[1] = GV.nodes[i].voltage.real * p[1]
+            p[2] = GV.nodes[i].voltage.imag * p[2]
+            q[1] = GV.nodes[i].voltage.imag * q[1]
+            q[2] = - GV.nodes[i].voltage.real * q[2]
+            imbalances_s.append(complex(sum(p), sum(q)))
+        return imbalances_s
+
+    @staticmethod
+    def get_imbalances_s_polar(y_matrix: numpy.ndarray) -> list:
+        """
+        Получение небалансов полной мощности узлов в полярных координатах
+        :param y_matrix: матрица собственных и взаимных проводимостей узлов
+        :return: список небалансов полной мощности узлов
+        """
+        node_count = len(GV.nodes)
+        imbalances_s = []
+        for i in range(node_count):
+            s_power = None
+            if GV.nodes[i].type_node == 'ИП':
+                s_power = complex(0, 0)
+            elif GV.nodes[i].type_node == 'ИОМ':
+                s_power = -GV.nodes[i].s_power
+            else:
+                s_power = GV.nodes[i].s_power
+            u_i = GV.nodes[i].get_polar_voltage()
+            b_i = GV.nodes[i].get_polar_angle()
+            p = [0, 0]
+            p[0] = s_power.real + y_matrix[i, i].real * u_i ** 2
+            q = [0, 0]
+            q[0] = s_power.imag - y_matrix[i, i].imag * u_i ** 2
+            for j in range(node_count):
+                if j != i:
+                    u_j = GV.nodes[j].get_polar_voltage()
+                    b_j = GV.nodes[j].get_polar_angle()
+                    p[1] += u_j * (y_matrix[i, j].real * cos(b_i - b_j) + y_matrix[i, j].imag * sin(b_i - b_j))
+                    q[1] += u_j * (y_matrix[i, j].real * sin(b_i - b_j) - y_matrix[i, j].imag * cos(b_i - b_j))
+            p[1] = u_i * p[1]
+            q[1] = u_i * q[1]
             imbalances_s.append(complex(sum(p), sum(q)))
         return imbalances_s
--- src/tests/calculation_tests/ImbalancesClass_tests.py
+++ src/tests/calculation_tests/ImbalancesClass_tests.py
@@ -3,15 +3,17 @@ from src.utils.FileManagerClass import FileManager
 import src.utils.GlobalVariables as GV
 from src.calculation.YMatrixClass import YMatrix
 
-filepath = "../data_tests/Data_Test_1.txt"
+filepath_1 = "../data_tests/Data_Test_1.txt"
+filepath_2 = "../data_tests/Data_Test_2.txt"
+
 
 
 class ImbalancesClassTests(unittest.TestCase):
     def test_get_imbalances_s_cartesian(self):
-        GV.OPTIONS = FileManager._FileManager__get_options(filepath)
-        GV.nodes = FileManager._FileManager__get_nodes(filepath)
-        GV.branches = FileManager._FileManager__get_branches(filepath)
-        GV.NEWTONMETHOD = FileManager._FileManager__get_newton_method(filepath)
+        GV.OPTIONS = FileManager._FileManager__get_options(filepath_1)
+        GV.nodes = FileManager._FileManager__get_nodes(filepath_1)
+        GV.branches = FileManager._FileManager__get_branches(filepath_1)
+        GV.NEWTONMETHOD = FileManager._FileManager__get_newton_method(filepath_1)
         GV.nodes[0].voltage = complex(115, 0)
 
         y_m = YMatrix.get_y_matrix()
@@ -19,7 +21,23 @@ class ImbalancesClassTests(unittest.TestCase):
 
         result = [complex(round(x.real, 3), round(x.imag, 3)) for x in imbalances_s]
 
-        expected_result = [complex(39.868, 39.321), complex(-38.855, -8.427)]
+        expected_result = [complex(-39.868, -39.321), complex(38.855, 8.427)]
+
+        self.assertEqual(result[1:], expected_result)
+
+    def test_get_imbalances_s_polar(self):
+        GV.OPTIONS = FileManager._FileManager__get_options(filepath_2)
+        GV.nodes = FileManager._FileManager__get_nodes(filepath_2)
+        GV.branches = FileManager._FileManager__get_branches(filepath_2)
+        GV.NEWTONMETHOD = FileManager._FileManager__get_newton_method(filepath_2)
+        GV.nodes[0].voltage = complex(115, 0)
+
+        y_m = YMatrix.get_y_matrix()
+        imbalances_s = GV.NEWTONMETHOD.imbalance_calculation(y_m)
+
+        result = [complex(round(x.real, 4), round(x.imag, 4)) for x in imbalances_s]
+
+        expected_result = [complex(-39.8675, -39.3205), complex(38.8547, 8.4273)]
 
         self.assertEqual(result[1:], expected_result)
 
