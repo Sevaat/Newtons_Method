255b104cc78eb52591cdaf70df83a00484bc5609
tkachenko <sevaatmail@gmail.com>
2025-01-10 18:23:16 +0500

    Добавление
    Тесты для расчета методом Ньютона в полярных координатах.

--- src/tests/NewtonMethod_tests.py
+++ src/tests/NewtonMethod_tests.py
@@ -3,29 +3,40 @@ from src.utils.FileManagerClass import FileManager
 import src.utils.GlobalVariables as GV
 from src.calculation.YMatrixClass import YMatrix
 from src.NewtonMethod import newton_method
+from math import *
 
-filepath = "data_tests/Data_Test_1.txt"
+filepath_1 = "data_tests/Data_Test_1.txt"
+filepath_2 = "data_tests/Data_Test_2.txt"
 
 
 class NewtonMethodTests(unittest.TestCase):
     def test_newton_method_cartesian(self):
-        GV.OPTIONS = FileManager._FileManager__get_options(filepath)
-        GV.nodes = FileManager._FileManager__get_nodes(filepath)
-        GV.branches = FileManager._FileManager__get_branches(filepath)
-        GV.NEWTONMETHOD = FileManager._FileManager__get_newton_method(filepath)
+        GV.OPTIONS = FileManager._FileManager__get_options(filepath_1)
+        GV.nodes = FileManager._FileManager__get_nodes(filepath_1)
+        GV.branches = FileManager._FileManager__get_branches(filepath_1)
+        GV.NEWTONMETHOD = FileManager._FileManager__get_newton_method(filepath_1)
         GV.nodes[0].voltage = complex(115, 0)
-
-        y_m = YMatrix.get_y_matrix()
-        i_s = GV.NEWTONMETHOD.imbalance_calculation(y_m)
-        j_m = GV.NEWTONMETHOD.jacoby_matrix_calculation(y_m)
-        d_v = GV.NEWTONMETHOD.linear_algebraic_equation_solver(i_s, j_m)
-        GV.NEWTONMETHOD.voltage_correction(d_v)
 
         newton_method()
 
         result = [round(abs(GV.nodes[1].voltage), 3), round(abs(GV.nodes[2].voltage), 3)]
 
         expected_result = [115.415, 109.721]
+
+        self.assertEqual(result, expected_result)
+
+    def test_newton_method_polar(self):
+        GV.OPTIONS = FileManager._FileManager__get_options(filepath_2)
+        GV.nodes = FileManager._FileManager__get_nodes(filepath_2)
+        GV.branches = FileManager._FileManager__get_branches(filepath_2)
+        GV.NEWTONMETHOD = FileManager._FileManager__get_newton_method(filepath_2)
+        GV.nodes[0].voltage = complex(115, 0)
+
+        newton_method()
+
+        result = [round(abs(GV.nodes[1].voltage), 3), round(abs(GV.nodes[2].voltage), 3)]
+
+        expected_result = [115.418, 109.717]
 
         self.assertEqual(result, expected_result)
 
